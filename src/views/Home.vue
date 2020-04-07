bbdd3e4e478278967a01f5bb645cecad084fce31
dc <dc870638530@163.com>
2020-04-07 23:26:36 +0800

    nextTick探索

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span ref='mg'>{{ msg }}</span>
+    <button @click='changeCon'>
+      点击改变文本
+    </button>
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return {
+      msg:'将要改变的信息'
+    }
+  },
+  methods:{
+    changeCon(){
+      // let that = this
+      this.$nextTick(() => {
+        
+        this.msg = '改变后的信息'
+      console.log(this.$refs.mg.innerText)
+      })
+    }
   }
 }
 </script>
